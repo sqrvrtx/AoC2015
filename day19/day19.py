9555a8523a3c8dd0862c7a3a2944839ce3338d58
Sqrvrtx <sqrvrtx@gmail.com>
2017-01-15 20:27:39 +0000

    test commit

---
+++ day19/day19.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+santa_molecule = """
+CRnSiRnCaPTiMgYCaPTiRnFArSiThFArCaSiThSiThPBCaCaSiRnSiRnTiTiMgArPBCaPMgYPTiRnFArFArCaSiRnBPMgArPRnCaPTiRnFArCaSiThCaCaFArPBCaCaPTiTiRnFArCaSiRnSiAlYSiThRnFArArCaSiRnBFArCaCaSiRnSiThCaCaCaFYCaPTiBCaSiThCaSiThPMgArSiRnCaPBFYCaCaFArCaCaCaCaSiThCaSiRnPRnFArPBSiThPRnFArSiRnMgArCaFYFArCaSiRnSiAlArTiTiTiTiTiTiTiRnPMgArPTiTiTiBSiRnSiAlArTiTiRnPMgArCaFYBPBPTiRnSiRnMgArSiThCaFArCaSiThFArPRnFArCaSiRnTiBSiThSiRnSiAlYCaFArPRnFArSiThCaFArCaCaSiThCaCaCaSiRnPRnCaFArFYPMgArCaPBCaPBSiRnFYPBCaFArCaSiAl"""
+
+
+def main(new_lines):
+    print(new_lines)
+
+if __name__ == "__main__":
+    with open('19.in', 'r') as f:
+        lines = f.read().splitlines()
+    new_lines = [x.split(' >= ') for x in lines]
+    main(new_lines)
